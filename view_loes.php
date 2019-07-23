13d52622d2f3e9ced5516df3cf54aff86eb1f641
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2019-07-23 15:22:00 +0200

    Fixed error of wrong method.

---
+++ view_loes.php
@@ -0,0 +1,57 @@
+<?php
+require_once(__DIR__ . '/lib/Content.php');
+require_once(__DIR__ . '/lib/Teacher.php');
+require_once(__DIR__ . '/lib/Semester.php');
+require_once(__DIR__ . '/lib/Year.php');
+require_once(__DIR__ . '/lib/Navigation.php');
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <?php include_once("inc/head.inc"); ?>
+    <script>
+        function show(elements, specifiedDisplay) {
+            var computedDisplay, element, index;
+
+            elements = elements.length ? elements : [elements];
+            for (index = 0; index < elements.length; index++) {
+                element = elements[index];
+
+                // Remove the element's inline display styling
+                element.style.display = '';
+                computedDisplay = window.getComputedStyle(element, null).getPropertyValue('display');
+
+                if (computedDisplay === 'none') {
+                    element.style.display = specifiedDisplay || 'block';
+                }
+            }
+        }
+    </script>
+</head>
+<body>
+
+<div class="container theme-showcase" role="main">
+
+    <?php
+    //fix new lp
+    if (isset($_GET["file"])) {
+        $lp = (new Teacher())->getSessionValue();
+        $ye = (new Year())->getSessionValue();
+        $sem = (new Semester())->getSessionValue();
+        $nav = new Navigation();
+
+        $file = $nav->getNewPath($_GET["file"],$lp,$ye,$sem);
+
+        if (strlen($file) > 0) {
+            $content = new Content();
+            $content->show($file);
+            echo "<script>show(document.querySelectorAll('.solution'));</script>";
+        }
+    }
+    ?>
+
+
+</div>
+</body>
+</html>
