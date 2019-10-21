1f44ff7b9578d7b960354e2cb76829119f6f9b25
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2019-10-21 16:13:52 +0200

    del: old comments/code

--- load.php
+++ load.php
@@ -2,25 +2,3 @@
 require_once(__DIR__ . '/lib/ContentView.php');
 $contentView = new ContentView();
 $contentView->showExamTests();
-/*
-$content = "";
-if (isset($_GET["file"])) {
-	$lp = $teacher->getSessionValue();
-    $file = Navigation::getNewPath($_GET["file"], $lp, null,null);
-    $content = file_get_contents($file);
-    if (isset($_GET["style"])) {
-    	$file2 = Navigation::getNewPath($_GET["style"], $lp, null,null);
-        $content = str_replace("style.css", $file2, $content);
-    }
-    if (isset($_GET["script"])) {
-    	$file2 = Navigation::getNewPath($_GET["script"], $lp, null,null);
-        $content = str_replace("script.js", $file2, $content);
-    }
-    if (isset($_GET["text"])) {
-    	$file2 = Navigation::getNewPath($_GET["text"], $lp, null,null);
-        $content = file_get_contents($file2);
-    }
-}
-//dump content
-printf("%s",$content);
-*/
